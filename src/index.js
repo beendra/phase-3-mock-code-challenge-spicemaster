f50ade1682d37b2ed8ba3778788fdcdf9f846ab3
beendra <alexandra.ricardel@gmail.com>
2021-03-24 11:04:45 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,37 +1,85 @@
 const url = "http://localhost:3000/spiceblends/"
 const body = document.body
+const formUpdate = body.querySelector('#update-form')
+const ingredientUpdate = body.querySelector('#ingredient-form')
 
 /*** FUNCTIONS ***/
 
-const renderOneSpice = (spiceObject) => {
+const renderOneSpice = (spiceObj) => {
     const detailImg = body.querySelector('#spice-blend-detail > img')    
-    detailImg.src = spiceObject.image
-    detailImg.alt = spiceObject.title
+    detailImg.src = spiceObj.image
+    detailImg.alt = spiceObj.title
 
     const detailH2 = body.querySelector('#spice-blend-detail > h2')
-    detailH2.textContent = spiceObject.title
+    detailH2.textContent = spiceObj.title
 
     const detailList = body.querySelector('#spice-blend-detail > div > ul')
 
     const iList = document.createElement('li')
-    iList.dataset.id = spiceObject.id
-    iList.innerText = spiceObject.ingredients.id
+    iList.dataset.id = spiceObj.id
+    iList.textContent = spiceObj.ingredients.id
 
     detailList.append(iList)
+
+    formUpdate.dataset.id = spiceObj.id
+    const newTitle = formUpdate.querySelector('#spiceblend-title')
+    const addIngredient = formUpdate.querySelector('#ingredient-name')
+    title.value = spiceObj.newTitle
+    ingredient.value = spiceObj.addIngredient
 }
 
-const renderFirstSpice = (spiceObject) => {
+const renderFirstSpice = (spiceObj) => {
     fetch (`${url}`)
     .then(resp => resp.json())
     .then(spiceObjs => {
-        spiceObjs.forEach(spiceObject => renderOneSpice(spiceObject))
+        spiceObjs.forEach(spiceObj => renderOneSpice(spiceObj))
     })
 }
-    
 
-// const imgSpice = (spiceObj) => {
-//     const img = document.createElement('img')
+const updateSpice = (event) => {
+    const id = event.target.dataset.id
+    event.preventDefault()
 
-// }
+    const newTitle = event.target[0].value
 
-renderFirstSpice()
+    fetch(`${url}/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            title: newTitle,
+        })
+    })
+}
+
+const addIngredient = (event) => {
+    const id = event.target.dataset.id
+    event.preventDefault()
+
+    const newIngredient = event.target[1].value
+
+    fetch(`${url}/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            ingredient: newIngredient
+        })
+    })
+
+}
+
+body.addEventListener('submit', event => {
+    if (event.target.matches('#update-form')){
+        updateSpice(event)
+    }
+    else if (event.target.matches('#ingredient-form')){
+        addIngredient(event)
+    }
+})
+
+renderFirstSpice();
